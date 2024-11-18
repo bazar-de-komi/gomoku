0824faf1a48e1aba92b56b6d7c5df9a98e7cff2b
Eric <eric1.xu@epitech.eu>
2024-11-18 13:14:35 +0100

    [FIX] compilation error due to return value not formatted correctly

--- src/AI/AI.cpp
+++ src/AI/AI.cpp
@@ -20,35 +20,36 @@ namespace gomoku
 
     std::vector<std::pair<int, int>> AI::_generate_possible_moves(const std::vector<std::vector<TableCell>> &board)
     {
-        return;
+        std::vector<std::pair<int, int>> possible_moves;
+        return possible_moves;
     }
     int AI::_simulate_random_game(std::vector<std::vector<TableCell>> board, TableCell current_player)
     {
-        return;
+        return 0;
     }
     bool AI::_check_horizontal(const std::vector<std::vector<TableCell>> &board)
     {
-        return;
+        return false;
     }
     bool AI::_check_vertical(const std::vector<std::vector<TableCell>> &board)
     {
-        return;
+        return false;
     }
     bool AI::_check_diagonal_to_down(const std::vector<std::vector<TableCell>> &board)
     {
-        return;
+        return false;
     }
     bool AI::_check_diagonal_to_up(const std::vector<std::vector<TableCell>> &board)
     {
-        return;
+        return false;
     }
     bool AI::_is_terminal(const std::vector<std::vector<TableCell>> &board)
     {
-        return;
+        return false;
     }
 
     std::string AI::play_ai_turn(const std::vector<std::vector<TableCell>> &board)
     {
-        return;
+        return "";
     }
 }
